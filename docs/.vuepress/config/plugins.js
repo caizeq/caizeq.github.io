ef8ced1da3a7409a204022fb365c6d769f365994
caizq <18859253934@139.com>
2021-04-15 10:36:10 +0800

    引入插件

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -1,8 +1,11 @@
+const musicplayer = require('./vuepress-plugin-musicplayer');
 const plugins = [
     // ['@vuepress/pwa', {
     //     serviceWorker: true, //用于缓存页面的内容以供离线使用
     //     updatePopup: true
     // }],
+    [ musicplayer, {}],
+    ['vuepress-plugin-live2d-model', {}],
     ['one-click-copy', {
         copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
         copyMessage: '复制成功~~', // default is 'Copy successfully and then paste it for use.'
---
+++ docs/.vuepress/config/vuepress-plugin-musicplayer/README.md
@@ -0,0 +1,25 @@
+# vuepress-plugin-musicplayer
+
+## How to use
+
+1. install `npm i vuepress-plugin-musicplayer -S`
+
+2. add in `config.js`
+
+```js
+module.exports = {
+  plugins: [
+    "vuepress-plugin-musicplayer",
+  ]
+}
+```
+
+## Tip
+
+- This plugin supports `vuepress 1.x`
+- This plugin will randomly change the white cat or black cat when router path change.
+
+## contact
+
+- 联系微信: qqlcx55
+- 微信公众号: 前端有道
